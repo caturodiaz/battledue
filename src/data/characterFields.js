b9b9a4059fee8a8fb78905a6d1fdc3dd3e7eb28c
Carlos Diaz <carlos.diaz@infracommcerce.lat>
2026-09-17 03:58:23 -0300

    Remove legacy character data

--- src/data/characterFields.js
+++
@@ -1,2 +0,0 @@
-export const characterFields = [['name','Nombre','text'],['image','Imagen (URL)','url'],['age','Edad','text'],['gender','Género','text'],['species','Especie','text'],['alignment','Alineación','text'],['power','Poder','text'],['weapon','Arma','text']]
-export const emptyCharacter = { name:'', image:'', age:'', gender:'', species:'', alignment:'', power:'', abilities:[], weapon:'' }
